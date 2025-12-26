fa2aba8a95311b2364984e4365a5dc4f541f7868
Tatyana Sidorenko <sta_78@list.ru>
2025-12-26 21:21:27 +0300

    new programma vihislenii

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        String text1 = new Scanner(System.in).nextLine();
+        int firstNumber = Integer.parseInt(text1);
+        System.out.println("Введите второе число: ");
+        String text2 = new Scanner(System.in).nextLine();
+        int secondNumber = Integer.parseInt(text2);
+
+        //вычисляем
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+
+        //выводим на экран
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
+
 
     }
 }
